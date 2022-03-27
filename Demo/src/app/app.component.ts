c17b164eb00978a39238024f55773712baec9eae
dtank <dtank150@rku.ac.in>
2022-03-27 20:04:14 +0530

    Demo

---
+++ Demo/src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Demo';
+
+  data=[
+    {
+      name:'Krishna',
+      age:20
+    },
+    {
+      name:'Shiv',
+      age:18
+    },
+    {
+      name:'Ram',
+      age:20
+    },
+    {
+      name:'Dev',
+      age:18
+    }
+  ]
+}
---
+++ Demo/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { IfComponent } from './if/if.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    IfComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
